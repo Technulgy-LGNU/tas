39d2d39a5b9c5e7fa23ebc05ab39813faa945c37
Elias Braun <braunelias@tghd.email>
2025-04-01 14:24:18 +0200

    Updated Email templates

--- src/mail/formEmail.go
+++ src/mail/formEmail.go
@@ -11,7 +11,8 @@ import (
 
 func SendEmailForm(email string, Name string, formEmail string, content string, cfg *config.CFG) error {
 	var (
-		to          = []string{email}
+		toSystem    = []string{email}
+		toUser      = []string{formEmail}
 		body        bytes.Buffer
 		mimeHeaders = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 
@@ -22,12 +23,29 @@ func SendEmailForm(email string, Name string, formEmail string, content string, 
 	body.Write([]byte(fmt.Sprintf("Subject: Technulgy Form \n%s\n\n", mimeHeaders)))
 
 	// Parse the template file
-	t, err := template.ParseFiles("templates/FormTemplate.html")
+	systemTemplate, err := template.ParseFiles("templates/FormSystemTemplate.html")
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error parsing template file: %v\n", err))
+	}
+	formEmailTemplate, err := template.ParseFiles("templates/FormTemplate.html")
 	if err != nil {
 		return errors.New(fmt.Sprintf("Error parsing template file: %v\n", err))
 	}
 
-	err = t.Execute(&body, struct {
+	err = systemTemplate.Execute(&body, struct {
+		Name    string
+		Email   string
+		Content string
+	}{
+		Name:    Name,
+		Email:   formEmail,
+		Content: content,
+	})
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error executing template: %v\n", err))
+	}
+
+	err = formEmailTemplate.Execute(&body, struct {
 		Name    string
 		Email   string
 		Content string
@@ -42,10 +60,15 @@ func SendEmailForm(email string, Name string, formEmail string, content string, 
 
 	// Authentication.
 	auth := smtp.PlainAuth("", cfg.Email.SenderEmail, cfg.Email.SenderEmailPassword, cfg.Email.Host)
-	// Sending email.
-	err = smtp.SendMail(cfg.Email.Host+":587", auth, cfg.Email.SenderEmail, to, body.Bytes())
+	// Sending emails.
+	err = smtp.SendMail(cfg.Email.Host+":587", auth, cfg.Email.SenderEmail, toSystem, body.Bytes())
 	if err != nil {
 		return errors.New(fmt.Sprintf("Error sending email: %v\n", err))
 	}
+	err = smtp.SendMail(cfg.Email.Host+":587", auth, cfg.Email.SenderEmail, toUser, body.Bytes())
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error sending email: %v\n", err))
+	}
+
 	return nil
 }
