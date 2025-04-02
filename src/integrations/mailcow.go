28dad622a21017c83206f9c75a404ae5bdd9bc22
Elias Braun <braunelias@tghd.email>
2025-04-02 16:28:08 +0200

    Before checkout

--- src/integrations/mailcow.go
+++ src/integrations/mailcow.go
@@ -3,6 +3,6 @@ package integrations
 import "tas/src/config"
 
 func CreateMailcowUser(userName string, userEmail string, password string, cfg *config.CFG) error {
-
+	// ToDo: Implement the function to create a user in Mailcow
 	return nil
 }
